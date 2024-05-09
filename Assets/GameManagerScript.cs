f9e8782b1f17ee92968558096b26b5b640d3049d
EPIKRAFTER\matth <hope.calvet@gmail.com>
2024-05-09 01:01:50 -0400

    Added sprites, died a little inside

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -2,9 +2,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+using Image = UnityEngine.UI.Image;
 
 public class GameManagerScript : MonoBehaviour
 {
+    public Image Corner;
+    public Image Selector;
+    public Sprite Red;
     public void Awake()
     {
         God.GM = this;
@@ -15,9 +20,12 @@ public class GameManagerScript : MonoBehaviour
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Selector.sprite = Red;
+        }
     }
 }
