dfb87db74198f80670599961ef74e21e3e040e7a
EPIKRAFTER\matth <hope.calvet@gmail.com>
2024-05-09 02:52:06 -0400

    Color Selector added

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -9,23 +9,184 @@ public class GameManagerScript : MonoBehaviour
 {
     public Image Corner;
     public Image Selector;
-    public Sprite Red;
+    public bool ColorMenu;
+    public int ColorMagic;
+    public List<Sprite> SelectorSprite;
+    public List<Sprite> CornerSprite;
     public void Awake()
     {
         God.GM = this;
     }
 
-    void Start()
+    public void Start()
     {
+        ColorMagic = 0;
+        ColorMenu = false;
+        ColorSelector();
         
     }
 
-    
-    void Update()
+    public void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            Selector.sprite = Red;
+            if (ColorMenu)
+            {
+                ColorMenu = false;
+                ColorSelector();
+            }
+            else
+            {
+                ColorMenu = true;
+                ColorSelector();
+            }
+        }
+
+        if (ColorMenu)
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                switch (ColorMagic)
+                {
+                    case 0:
+                        Selector.sprite = SelectorSprite[3];
+                        Corner.sprite = CornerSprite[3];
+                        ColorMagic = 3;
+                        break;
+                    case 1:
+                        Selector.sprite = SelectorSprite[2];
+                        Corner.sprite = CornerSprite[2];
+                        ColorMagic = 2;
+                        break;
+                    case 2:
+                        Selector.sprite = SelectorSprite[3];
+                        Corner.sprite = CornerSprite[3];
+                        ColorMagic = 3;
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        Selector.sprite = SelectorSprite[2];
+                        Corner.sprite = CornerSprite[2];
+                        ColorMagic = 2;
+                        break;
+                    case 5:
+                        Selector.sprite = SelectorSprite[1];
+                        Corner.sprite = CornerSprite[1];
+                        ColorMagic = 1;
+                        break;
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                switch (ColorMagic)
+                {
+                    case 0:
+                        Selector.sprite = SelectorSprite[1];
+                        Corner.sprite = CornerSprite[1];
+                        ColorMagic = 1;
+                        break;
+                    case 1:
+                        Selector.sprite = SelectorSprite[5];
+                        Corner.sprite = CornerSprite[5];
+                        ColorMagic = 5;
+                        break;
+                    case 2:
+                        Selector.sprite = SelectorSprite[1];
+                        Corner.sprite = CornerSprite[1];
+                        ColorMagic = 1;
+                        break;
+                    case 3:
+                        Selector.sprite = SelectorSprite[2];
+                        Corner.sprite = CornerSprite[2];
+                        ColorMagic = 2;
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                switch (ColorMagic)
+                {
+                    case 0:
+                        Selector.sprite = SelectorSprite[5];
+                        Corner.sprite = CornerSprite[5];
+                        ColorMagic = 5;
+                        break;
+                    case 1:
+                        Selector.sprite = SelectorSprite[5];
+                        Corner.sprite = CornerSprite[5];
+                        ColorMagic = 5;
+                        break;
+                    case 2:
+                        Selector.sprite = SelectorSprite[1];
+                        Corner.sprite = CornerSprite[1];
+                        ColorMagic = 1;
+                        break;
+                    case 3:
+                        Selector.sprite = SelectorSprite[5];
+                        Corner.sprite = CornerSprite[5];
+                        ColorMagic = 5;
+                        break;
+                    case 4:
+                        Selector.sprite = SelectorSprite[2];
+                        Corner.sprite = CornerSprite[2];
+                        ColorMagic = 2;
+                        break;
+                    case 5:
+                        break;
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                switch (ColorMagic)
+                {
+                    case 0:
+                        Selector.sprite = SelectorSprite[4];
+                        Corner.sprite = CornerSprite[3];
+                        ColorMagic = 4;
+                        break;
+                    case 1:
+                        Selector.sprite = SelectorSprite[2];
+                        Corner.sprite = CornerSprite[2];
+                        ColorMagic = 2;
+                        break;
+                    case 2:
+                        Selector.sprite = SelectorSprite[4];
+                        Corner.sprite = CornerSprite[4];
+                        ColorMagic = 4;
+                        break;
+                    case 3:
+                        Selector.sprite = SelectorSprite[4];
+                        Corner.sprite = CornerSprite[4];
+                        ColorMagic = 4;
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        Selector.sprite = SelectorSprite[1];
+                        Corner.sprite = CornerSprite[1];
+                        ColorMagic = 1;
+                        break;
+                }
+            }
+        }
+    }
+
+    public void ColorSelector()
+    {
+        if (ColorMenu)
+        {
+            Selector.color = Color.white;
+            Corner.color = Color.clear;
+        }
+        else
+        {
+            Selector.color = Color.clear;
+            Corner.color = Color.white;
         }
     }
 }
--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -9,8 +9,6 @@ public class PlayerScript : MonoBehaviour
     public Rigidbody RB;
     public float MouseSensitivity = 3;
     public float WalkSpeed = 10;
-    public float JumpPower = 7;
-    public List<GameObject> Floors;
 
     public void Awake()
     {
@@ -23,66 +21,47 @@ public class PlayerScript : MonoBehaviour
     }
     void Update()
     {
-        float xRot = Input.GetAxis("Mouse X") * MouseSensitivity;
-        float yRot = -Input.GetAxis("Mouse Y") * MouseSensitivity;
-        transform.Rotate(0,xRot,0);
-
-        Vector3 eRot = Eyes.transform.localRotation.eulerAngles;
-        eRot.x += yRot;
-
-        if (eRot.x < -180)
+        if (God.GM.ColorMenu == false)
         {
-            eRot.x += 360;
-        }
+            float xRot = Input.GetAxis("Mouse X") * MouseSensitivity;
+            float yRot = -Input.GetAxis("Mouse Y") * MouseSensitivity;
+            transform.Rotate(0,xRot,0);
 
-        if (eRot.x > 180)
-        {
-            eRot.x -= 360;
-        }
+            Vector3 eRot = Eyes.transform.localRotation.eulerAngles;
+            eRot.x += yRot;
 
-        eRot = new Vector3(Mathf.Clamp(eRot.x,-90,90),0,0);
-        Eyes.transform.localRotation = Quaternion.Euler(eRot);
-
-        if (WalkSpeed > 0)
-        {
-            Vector3 move = Vector3.zero;
-            if (Input.GetKey(KeyCode.W))
-                move += transform.forward;
-            if (Input.GetKey(KeyCode.S))
-                move -= transform.forward;
-            if (Input.GetKey(KeyCode.A))
-                move -= transform.right;
-            if (Input.GetKey(KeyCode.D))
-                move += transform.right;
-            move = move.normalized * WalkSpeed;
-            if (JumpPower > 0 && Input.GetKeyDown(KeyCode.Space) && OnGround())
+            if (eRot.x < -180)
             {
-                move.y = JumpPower;
+                eRot.x += 360;
             }
-            else
+
+            if (eRot.x > 180)
             {
-                move.y = RB.velocity.y;
+                eRot.x -= 360;
             }
-            RB.velocity = move;
+
+            eRot = new Vector3(Mathf.Clamp(eRot.x,-90,90),0,0);
+            Eyes.transform.localRotation = Quaternion.Euler(eRot);
+
+            if (WalkSpeed > 0)
+            {
+                Vector3 move = Vector3.zero;
+                if (Input.GetKey(KeyCode.W))
+                    move += transform.forward;
+                if (Input.GetKey(KeyCode.S))
+                    move -= transform.forward;
+                if (Input.GetKey(KeyCode.A))
+                    move -= transform.right;
+                if (Input.GetKey(KeyCode.D))
+                    move += transform.right;
+                move = move.normalized * WalkSpeed;
+            
+                RB.velocity = move;
+            }
         }
-    }
-
-    public bool OnGround()
-    {
-        return Floors.Count > 0;
-    }
-
-    private void OnCollisionEnter(Collision other)
-    {
-        if (!Floors.Contains(other.gameObject))
+        else
         {
-            Floors.Add(other.gameObject);
+            RB.velocity = Vector3.zero;
         }
     }
-
-    private void OnCollisionExit(Collision other)
-    {
-        Floors.Remove(other.gameObject);
-    }
-
 }
